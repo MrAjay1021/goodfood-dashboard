10405effb73e573304e359941fe323629049e2a9
MrAjay1021 <ajay.prasad1021@gmail.com>
2025-12-24 17:36:48 +0530

    Jest configuration and twoSum tests

--- utils/twoSum.js
+++ utils/twoSum.js
@@ -1,21 +1,25 @@
 /**
- * twoSum - Returns indices of two numbers that add up to target
- * @param nums - Array of numbers
- * @param target - Target sum
- * @returns Array of two indices, or empty array if no solution exists
+ * twoSum - Find indices of two numbers that add up to target
+ * @param {number[]} nums - Array of numbers
+ * @param {number} target - Target sum
+ * @returns {number[]} - Indices of two numbers adding to target, or empty array if none exist
  */
-export function twoSum(nums: number[], target: number): number[] {
-  const numMap = new Map<number, number>()
+function twoSum(nums, target) {
+  // Using a hash map for O(n) time complexity
+  const numMap = new Map()
 
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i]
 
     if (numMap.has(complement)) {
-      return [numMap.get(complement)!, i]
+      return [numMap.get(complement), i]
     }
 
     numMap.set(nums[i], i)
   }
 
+  // If no pair found, return empty array
   return []
 }
+
+export default twoSum
