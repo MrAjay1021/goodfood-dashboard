6c1cc9a7deec2b898a633295b4efb43c6b797a93
MrAjay1021 <ajay.prasad1021@gmail.com>
2025-12-24 17:43:33 +0530

    sidebar icons

---
+++ components/icons/food-icons.jsx
@@ -0,0 +1,5 @@
+// Food item SVG icons for Most Ordered Card
+
+export function SaladIcon({ className }) {
+  return <img src="/images/most-ordered-card-salad.svg" alt="Fresh Salad Bowl" className={className} />
+}
---
+++ components/icons/header-icons.jsx
@@ -0,0 +1,77 @@
+// Header SVG icons
+
+export function SearchIcon({ className }) {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g opacity="0.5">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M5.66314 10.5763C8.3766 10.5763 10.5763 8.3766 10.5763 5.66314C10.5763 2.94969 8.3766 0.75 5.66314 0.75C2.94969 0.75 0.75 2.94969 0.75 5.66314C0.75 8.3766 2.94969 10.5763 5.66314 10.5763Z"
+          stroke="#627B87"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9.4693 9.46889L12.5574 12.557"
+          stroke="#627B87"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export function NotificationIcon({ className }) {
+  return (
+    <svg
+      width="13"
+      height="16"
+      viewBox="0 0 13 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M6.4 16C5.51684 15.9957 4.80073 15.2831 4.792 14.4H7.992C7.99369 14.6139 7.9529 14.826 7.872 15.024C7.66212 15.5056 7.23345 15.8569 6.72 15.968H6.716H6.704H6.6896H6.6824C6.58945 15.9874 6.49492 15.9981 6.4 16ZM12.8 13.6H0V12L1.6 11.2V6.8C1.55785 5.67129 1.81275 4.5513 2.3392 3.552C2.86323 2.62521 3.75896 1.9671 4.8 1.744V0H8V1.744C10.0632 2.2352 11.2 4.0304 11.2 6.8V11.2L12.8 12V13.6Z"
+        fill="#B0C3CC"
+      />
+    </svg>
+  )
+}
+
+export function ArrowDownIcon({ className }) {
+  return (
+    <svg width="11" height="7" viewBox="0 0 11 7" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path
+        d="M5.00833 6.1875L10.0167 1.17917L8.83917 0L5.00833 3.83333L1.17833 0L0 1.17833L5.00833 6.1875Z"
+        fill="#1F384C"
+      />
+    </svg>
+  )
+}
+
+export function MenuIcon({ className }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path d="M3 12H21M3 6H21M3 18H21" stroke="#4B5563" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
+}
